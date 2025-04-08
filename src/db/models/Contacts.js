76f7c27c21460b268cd3a038ac45edf3f4871fed
Sofiia <sonya17035@gmail.com>
2025-04-08 23:03:01 +0200

    fix

--- src/db/models/Contacts.js
+++ src/db/models/Contacts.js
@@ -13,7 +13,7 @@ const contactsSchema = new Schema(
         email: {
             type: String,
         },
-        isFavorite: {
+        isFavourite: {
             type: Boolean,
             default: false,
         },
